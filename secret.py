1719907f4df8311bac2d68c8784aa1557ac5315a
jukaiof <36453238+jukaiof@users.noreply.github.com>
2020-07-28 16:14:06 +0900

    Delete secret.py

--- secret.py
+++
@@ -1,8 +0,0 @@
-token = "NzI1ODk3NjEwOTIxMjQ2ODEy.XvVbeg.1bOooW_Jzntn-a4pX4UxL5iF6zg"
-token2 = "NzEyNzYyMTYyMjMzNDc1MTY0.XsWRfQ._rsvlprVOkg8hT6Z5DXRKOaZRVY"
-c_list = []
-text = []
-a = []
-number = []
-counter = 0
-bot_on = []
